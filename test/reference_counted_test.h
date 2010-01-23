169beaa97a17bb2e1b979b3ea92da241b329218b
Gaspard Bucher <gaspard@teti.ch>
2010-01-23 23:02:34 +0100

    Added tests for ReferenceCounted class.

--- test/reference_counted_test.h
+++ test/reference_counted_test.h
@@ -27,49 +27,53 @@
   ==============================================================================
 */
 
-#ifndef OSCIT_INCLUDE_REFERENCE_COUNTED_H_
-#define OSCIT_INCLUDE_REFERENCE_COUNTED_H_
+#include "test_helper.h"
+#include "mock/ref_counted_logger.h"
 
-namespace oscit {
+class ReferenceCountedTest : public TestHelper {
+public:
 
-/** Maintains a reference count of an element.
- * When the reference count reaches zero, the object is deleted.
- * Note that this class *is not thread-safe*. You should use
- * ConcurrentReferenceCounted for a thread-safe version.
- */
-class ReferenceCounted {
- public:
-  ReferenceCounted() : ref_count_(1) {}
+  void test_ref_count_should_start_at_one(void) {
+    Logger logger;
+    RefCountedLogger obj("ref", &logger);
 
-  virtual ~ReferenceCounted() {}
-
-  size_t ref_count() { return ref_count_; }
-
-  template<class T>
-  static T* acquire(T *elem) {
-    ++elem->ref_count_;
-    return elem;
+    assert_equal(1, obj.ref_count());
   }
 
-  template<class T>
-  static T* release(T *elem) {
-    if (elem) {
-      if (--elem->ref_count_ == 0) delete elem;
-    }
-    return NULL;
+  void test_ref_count_should_increase_on_acquire_or_retain(void) {
+    Logger logger;
+    RefCountedLogger obj("ref", &logger);
+    ReferenceCounted::acquire(&obj);
+    assert_equal(2, obj.ref_count());
+
+    obj.retain();
+    assert_equal(3, obj.ref_count());
   }
 
-  void retain() {
-    ++ref_count_;
+  void test_ref_count_should_decrease_on_release(void) {
+    Logger logger;
+    RefCountedLogger obj("ref", &logger);
+    ReferenceCounted::acquire(&obj);
+    ReferenceCounted::acquire(&obj);
+    assert_equal(3, obj.ref_count());
+
+    ReferenceCounted::release(&obj);
+    assert_equal(2, obj.ref_count());
+
+    obj.release();
+    assert_equal(1, obj.ref_count());
   }
 
-  void release() {
-    if (--ref_count_ == 0) delete this;
+  void test_delete_on_ref_count_to_zero(void) {
+    Logger logger;
+    RefCountedLogger *obj = new RefCountedLogger("ref", &logger);
+
+    ReferenceCounted::release(obj);
+    assert_equal("[ref: destroyed]", logger.str());
+    logger.str("");
+
+    obj = new RefCountedLogger("ref", &logger);
+    obj->release();
+    assert_equal("[ref: destroyed]", logger.str());
   }
- protected:
-  size_t ref_count_;
 };
-
-} // oscit
-
-#endif // OSCIT_INCLUDE_REFERENCE_COUNTED_H_
